b9db66a6ef1744b1a7a9830e7ce95d98e8b3ee47
96glory <click_syk@naver.com>
2020-08-26 23:24:16 +0900

    Update on 200826

--- acmicpc/10971.cpp
+++ acmicpc/10971.cpp
@@ -1,22 +1,47 @@
-#include <stdio.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int N;
-	scanf("%d", &N);
-	
-	int arr[N][N];
-	int W[N][N];
-	
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			scanf("%d", &arr[i][j]);
-			W[i][j] = arr[i][j];
-		}
-	}
+int n, arr[11][11], visit[11], answer = 2147000000;
 
-	
-	
+
+void dfs(int start, int i, int sum, int cnt) {
+    if (start == i && cnt == n) {
+        answer = min(answer, sum);
+        return;
+    }
+    else {
+        for (int j = 1; j <= n; j++) {
+            if (arr[i][j] == 0)  continue;
+
+            if (!visit[i] && arr[i][j] > 0) {
+                visit[i] = true;
+                sum += arr[i][j];
+
+                if (sum <= answer)
+                    dfs(start, j, sum, cnt + 1);
+
+                visit[i] = false;
+                sum -= arr[i][j];
+            }
+        }
+    }
 }
 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        dfs(i, i, 0, 0);
+    }
+
+    cout << answer;
+}
