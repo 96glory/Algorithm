1d425404da46a1597bfbe371b64533bdeb1022ee
96glory <52440668+YoungKwang-Song@users.noreply.github.com>
2020-09-05 11:21:33 +0900

    Update on 200905

---
+++ acmicpc/16916.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char a[1000001], b[1000001];
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+
+	cin >> a >> b;
+	if (strstr(a, b) == NULL)   cout << 0;
+	else                		cout << 1;
+}
